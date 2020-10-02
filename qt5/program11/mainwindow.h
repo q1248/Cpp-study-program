154b2604038585d8fe6b2f24fcf973755599dda7
aknightive <1248597254@qq.com>
2020-10-02 09:24:46 +0800

    update

--- qt5/program11/mainwindow.h
+++ qt5/program11/mainwindow.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QMainWindow>
+#include<QLabel>
+#include<QTreeWidgetItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,7 +16,30 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_exit_triggered();
+
+    void on_add_dic_triggered();
+
+    void on_action_2_triggered();
+
+    void on_treeWidget_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
+
+    void on_del_node_triggered();
+
 private:
     Ui::MainWindow *ui;
-    void init();
+    enum treeItemType{itTopItem=1001,itGroupItem,itImageItem};
+    enum treeColNum{colItem=0,colItemType=1};
+
+    QLabel *statusShow;
+    QPixmap curPixMap;
+    float pixRate;
+
+    void init_tree();
+    void addFloder(QTreeWidgetItem *parItem,QString dirName);
+    QString getFinalFloderName(const QString &full_dir_name);
+    void addImagesItem(QTreeWidgetItem *parItem,QString aFileName);
+    void displayImage(QTreeWidgetItem *parItem);
+    void changeItemCaption(QTreeWidgetItem *item);
 };
